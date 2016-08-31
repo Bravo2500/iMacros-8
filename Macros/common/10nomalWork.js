1cdf9d3aad3dba40e6d0c5ba0b11082e5b132081
terashi <terashi@spookies.co.jp>
2016-09-01 04:04:57 +0900

    Move directory to control git repository

---
+++ Macros/common/10nomalWork.js
@@ -0,0 +1,16 @@
+﻿var nonStaminaString;
+
+var needString;
+
+for(var i=0;i<1000;i++){
+  iimPlay("10nomalWork");
+
+  iimPlay("checkStamina");
+
+  needString=iimGetLastExtract(1);
+ 
+  if (needString.indexOf('スタミナがなくなった')!= -1){
+    needString="";
+    iimPlay("useHarfStaminaDrink");
+  }
+}
---
+++ idle_produce.js
@@ -0,0 +1,27 @@
+﻿var nonStaminaString;
+
+var needString="";
+
+var needImage="";
+for(var i=0;i<100000;i++){
+    iimPlay("produce");
+
+    iimPlay("isAppearCommunication");
+    needImage = iimGetLastExtract(1);
+
+    if (needImage.indexOf('button_normal_03')!= -1){
+        needString="";
+        iimPlay("nomal_communication");
+        continue;
+    }
+
+    iimPlay("checkStamina");
+    needString=iimGetLastExtract(1);
+
+    if (needString.indexOf('スタミナがなくなった')!= -1){
+        needString="";
+        iimPlay("useHarfStaminaDrink");
+        continue;
+    }
+
+}
